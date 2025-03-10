7d22b57850ee3c575ca12f8ad6f20fe6700f51c3
João Gabriel <jgcf706@gmail.com>
2025-03-10 17:21:56 -0300

    Add files via upload

---
+++ estudos 5.py
@@ -0,0 +1,4 @@
+fnam = input("Posso ter seu primeiro nome, por favor? ")
+lnam = input("Posso ter seu sobrenome, por favor? ")
+print("Obrigado!.")
+print("\nSeu nome é " + fnam + " " + lnam + ".")
---
+++ estudos 6.py
@@ -0,0 +1,9 @@
+# entre com um valor float para a variável a aqui
+arm_a = float(input("Insira a Medida do Seu Braço Direito: "))
+# entre com um valor float para a variável b aqui
+arm_b = float(input("Insira a Medida do Seu Braço Esquerdo: "))
+# imprima o resultado da adição aqui
+print("A Soma dos Dois Braços é " + str(arm_a + arm_b))
+
+print("\nIsso é tudo, pessoal!")
+
